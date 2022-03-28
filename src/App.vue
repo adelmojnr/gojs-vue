a7fc0be00fffa3fe012f285379129ace49067de4
Adelmo Júnior <junior-adelmo@hotmail.com>
2022-03-28 06:11:58 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+
+<div id="app">
+<Graph /> 
+</div>
+</template>
+<script>
+import Graph from './components/Graph.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Graph
+  }
+}
+</script>
+<style>
+
+</style>
